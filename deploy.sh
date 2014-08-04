cd09d165107df802159e05c9c0afcac82e1f4cff
YinChunxiang <yinchunxiang@gmail.com>
2014-08-04 18:57:57 +0800

    modify snipmate

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 cd "$(dirname $0)"
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-if [ ! -f ~/.vimrc ];then
-    cp .vimrc .gvimrc ~/
-fi
-if [ ! -f ~/.vim/bundle ];then
-    mkdir -p ~/.vim
-    cp -r bundle ~/.vim
-fi
+git clone http://github.com/gmarik/vundle.git ./bundle/vundle
 vim +BundleInstall +qa
