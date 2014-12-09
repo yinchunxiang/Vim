0edf9df7d42d6149845e45e8cdc40caee25b7d2a
yinchunxiang <yinchunxiang@gmail.com>
2014-12-08 19:26:32 -0800

    aa

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.vim
 cd "$(dirname $0)"
-git clone http://github.com/gmarik/vundle.git ./bundle/vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qa
