c0fa0582ce3419e1f7b2f7dbff973979a3e93396
yinchunxiang <yinchunxiang@gmail.com>
2014-12-24 04:42:09 -0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 mkdir -p ~/.vim
+
+#enter the directory of this script
 cd "$(dirname $0)"
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qa
-ln -s ~/Github/Vim/vimrc ~/.vimrc 
+
+#git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#vim +BundleInstall +qa
+rm -f ~/.vimrc
+ln -s vimrc ~/.vimrc 
+
+rm -rf ~/.vim
+ln -s ../Vim ~/.vim
